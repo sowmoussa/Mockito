4d745e5b1d0b2cdf8864b9dba482898bff7196a6
Moussa SOW <moussa4.sow@gmail.com>
2021-11-16 18:17:21 +0100

    [Mockito]: first test :-)

---
+++ src/test/java/io/sowmoussa/FirstMockitoTest.java
@@ -0,0 +1,13 @@
+package io.sowmoussa;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FirstMockitoTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
