5f1bbd6cf47930cd3e9fac5382c4beb68c7cead6
Moussa SOW <moussa4.sow@gmail.com>
2021-11-18 13:43:12 +0100

    [Mockito]: Arg matchers and throwing exception

--- src/test/java/io/sowmoussa/business/ListTest.java
+++ src/test/java/io/sowmoussa/business/ListTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -15,5 +16,29 @@ public class ListTest {
         List listMock = mock(List.class);
         when(listMock.size()).thenReturn(2);
         assertEquals(2, listMock.size());
+    }
+
+    @Test
+    public void ListMockTest_ReturnMultipleValues() {
+        List listMock = mock(List.class);
+        when(listMock.size()).thenReturn(2).thenReturn(3);
+        assertEquals(2, listMock.size());
+        assertEquals(3, listMock.size());
+    }
+
+    @Test
+    public void ListMockGetTest() {
+        List listMock = mock(List.class);
+        // argument matcher
+        when(listMock.get(anyInt())).thenReturn("moscar");
+        assertEquals("moscar", listMock.get(0));
+        assertEquals("moscar", listMock.get(2));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void ListMockGetTest_throwException() {
+        List listMock = mock(List.class);
+        when(listMock.get(anyInt())).thenThrow(new RuntimeException());
+        listMock.get(0);
     }
 }
